f8ec8505c9955f005fea73e87813cb5ce3633ada
ozy24 <24205522+ozy24@users.noreply.github.com>
2026-06-11 23:53:26 +0100

    Fix Weapon_RunThink null-weapon crash; raise g_horde_player_scale_max default to 8

--- src/g_local.h
+++ src/g_local.h
@@ -11,7 +11,7 @@
 constexpr const char *GAMEVERSION = "baseq2";
 
 constexpr const char *GAMEMOD_TITLE = "Muff Mode";
-constexpr const char *GAMEMOD_VERSION = "0.30.00 BETA";
+constexpr const char *GAMEMOD_VERSION = "0.33.02 BETA";
 
 //==================================================================
 
